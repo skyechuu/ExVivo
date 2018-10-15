f180c6ac9700bdf71f346f80b76c050cc3250417
Gokhan Nas <n.gokhan36@gmail.com>
2018-10-15 15:13:53 +0300

    Project Refactor, UFPS Added, Development Scene added

---
+++ Assets/Scripts/Controllers/InteractionController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionController : MonoBehaviour {
+
+    [SerializeField] LayerMask interactionLayers;
+    [SerializeField] float interactionDistance;
+
+    private Ray ray;
+    private RaycastHit hit;
+
+    void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        ray = new Ray(transform.position, transform.forward);
+        if(Physics.Raycast(ray, out hit, interactionDistance, interactionLayers))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
